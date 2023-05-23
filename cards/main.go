3f3d8331c7389894e6c5ccb7e8e6b61ca5e3a2c2
Senuch <s.uzairtariq@gmail.com>
2023-05-24 01:18:53 +0500

    Save deck to file

--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
-	/*cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 5)
-
-	hand.print()
-	remainingDeck.print()*/
 	cards := newDeck()
-	fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
