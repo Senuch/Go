618c95c984b1c9d1900a07d27a274c366a2f3661
Senuch <s.uzairtariq@gmail.com>
2023-05-25 20:29:08 +0500

    Other ways of creating map, deleting value from map

--- maps/main.go
+++ maps/main.go
@@ -3,11 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
+	/*colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#ff0001",
 		"blue":  "#ff0002",
-	}
+	}*/
+
+	//var colors map[string]string
+	colors := make(map[string]string)
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
