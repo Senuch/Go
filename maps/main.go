ac3242689ec132601b3dbb9885ef9b36ab72e31c
Senuch <s.uzairtariq@gmail.com>
2023-05-25 20:32:55 +0500

    Using iteration on map

--- maps/main.go
+++ maps/main.go
@@ -3,17 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	/*colors := map[string]string{
+	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#ff0001",
 		"blue":  "#ff0002",
-	}*/
+	}
 
 	//var colors map[string]string
-	colors := make(map[string]string)
+	/*colors := make(map[string]string)
 	colors["white"] = "#ffffff"
 
-	delete(colors, "white")
+	delete(colors, "white")*/
 
-	fmt.Println(colors)
+	//fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for ", color, " is ", hex)
+	}
 }
